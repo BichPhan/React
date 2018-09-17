5cfbc8c5b40a0ade617144c33b7dd7870b667725
BichPhan <40346768+BichPhan@users.noreply.github.com>
2018-09-17 10:42:43 +0700

    17/09/2018

--- setting_color/src/index.js
+++ setting_color/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
